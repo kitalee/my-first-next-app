37a3e7c38c1db438365762c21b78b814a3e4fc83
Donggeon Lee <donggeon.lee@keymedia.com>
2022-09-14 16:49:44 +1000

    332. The "_app.js" File & Layout Wrapper

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,34 @@
-const HomePage = () => {
-  return <h1>This is home</h1>
-}
+import MeetupList from "../components/meetups/MeetupList";
 
-export default HomePage
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A first meetup",
+    image:
+      "https://wilstar.com/wp-content/uploads/2020/12/new-years-eve-1953253_640.jpg",
+    address: "test address1",
+    description: "test desc1",
+  },
+  {
+    id: "m2",
+    title: "A second meetup",
+    image:
+      "https://wilstar.com/wp-content/uploads/2020/12/new-years-eve-1953253_640.jpg",
+    address: "test address2",
+    description: "test desc2",
+  },
+  {
+    id: "m3",
+    title: "A third meetup",
+    image:
+      "https://wilstar.com/wp-content/uploads/2020/12/new-years-eve-1953253_640.jpg",
+    address: "test address33",
+    description: "test desc3",
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
+
+export default HomePage;

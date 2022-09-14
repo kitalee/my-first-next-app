9a7c6ba0467d84f586b2a50cb50bb9a49b4a6236
Donggeon Lee <donggeon.lee@keymedia.com>
2022-09-14 17:40:51 +1000

    334. Adding Custom Components & CSS Modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://wilstar.com/wp-content/uploads/2020/12/new-years-eve-1953253_640.jpg"
+      title="a first meetup"
+      address="address"
+      description="desc"
+    />
+  );
+};
+
+export default MeetupDetails;
